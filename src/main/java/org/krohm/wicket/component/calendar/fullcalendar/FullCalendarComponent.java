078075e344cab3e31059c851ef1fbc2f0a95ed9a
unknown <arnaud@.(none)>
2010-06-18 16:40:17 +0200

    upgraded Calendar display

--- src/main/java/org/krohm/wicket/component/calendar/fullcalendar/FullCalendarComponent.java
+++ src/main/java/org/krohm/wicket/component/calendar/fullcalendar/FullCalendarComponent.java
@@ -6,6 +6,7 @@ package org.krohm.wicket.component.calendar.fullcalendar;
 
 import org.apache.wicket.markup.html.JavascriptPackageResource;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.html.resources.StyleSheetReference;
 
 /**
  *
@@ -17,11 +18,14 @@ public class FullCalendarComponent extends Panel {
     private static final String FC_SCRIPT_NAME = "fullcalendar.min.js";
     private static final String JQUERY_SCRIPT_NAME = "jquery.js";
     private static final String JQUERY_UI_SCRIPT_NAME = "jquery-ui-custom.js";
+    private static final String FX_CSS_NAME = "fullcalendar.css";
 
     public FullCalendarComponent(String id) {
         super(id);
         add(JavascriptPackageResource.getHeaderContribution(FullCalendarComponent.class, JQUERY_SCRIPT_NAME));
         add(JavascriptPackageResource.getHeaderContribution(FullCalendarComponent.class, JQUERY_UI_SCRIPT_NAME));
         add(JavascriptPackageResource.getHeaderContribution(FullCalendarComponent.class, FC_SCRIPT_NAME));
+        //add(JavascriptPackageResource.getHeaderContribution(FullCalendarComponent.class, FX_CSS_NAME));
+
     }
 }
