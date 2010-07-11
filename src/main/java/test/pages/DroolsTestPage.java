0303ce15f0cfb0eeca7a2acb1203528acb56736c
Krohm <arnaud.demarcq@laposte.net>
2010-07-11 15:00:49 +0200

    Added sample for Excel input file compilation*

--- src/main/java/test/pages/DroolsTestPage.java
+++ src/main/java/test/pages/DroolsTestPage.java
@@ -5,6 +5,8 @@
 package test.pages;
 
 import beans.Message;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Collection;
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
@@ -15,6 +17,8 @@ import org.drools.builder.KnowledgeBuilderFactory;
 import org.drools.builder.ResourceType;
 import org.drools.conflict.FifoConflictResolver;
 import org.drools.conflict.SalienceConflictResolver;
+import org.drools.decisiontable.InputType;
+import org.drools.decisiontable.SpreadsheetCompiler;
 import org.drools.definition.KnowledgePackage;
 import org.drools.event.rule.DebugAgendaEventListener;
 import org.drools.event.rule.DebugWorkingMemoryEventListener;
@@ -30,21 +34,36 @@ import org.slf4j.LoggerFactory;
  */
 public class DroolsTestPage extends PocMainPage {
 
+    //private static  final String testExcelFileName = "test/pages/ExampleMortgageLoan.xls";
+    private static final String testExcelFileName = "ExampleMortgageLoan.xls";
     final KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
     private static final Logger logger = LoggerFactory.getLogger(DroolsTestPage.class);
     private ConflictResolver[] conflictResolvers = new ConflictResolver[]{
         SalienceConflictResolver.getInstance(),
         FifoConflictResolver.getInstance()};
+    private SpreadsheetCompiler compiler = new SpreadsheetCompiler();
 
-    public DroolsTestPage(PageParameters parameters) {
+    public DroolsTestPage(PageParameters parameters) throws Exception {
         super(parameters);
+        // TEST File Compilation
+
+        String tmpString = DroolsTestPage.class.getResource(testExcelFileName).toString();
+        tmpString = "C:/Users/arnaud/GaeTest/MyGaeTest/MyGaeTest/target/classes/test/pages/ExampleMortgageLoan.xls";
+        tmpString = "C:/Users/arnaud/GaeTest/MyGaeTest/MyGaeTest/target/classes/test/pages/DecisionTablesSample.xls";
+        FileInputStream fis = new FileInputStream(tmpString);
+        String drl = compiler.compile(fis, InputType.XLS);
+        fis.close();
+        logger.error(drl);
+
+
 
         // tries ... Something
         kbuilder.add(ResourceFactory.newClassPathResource("HelloWorld.drl",
                 DroolsTestPage.class), ResourceType.DRL);
         kbuilder.add(ResourceFactory.newClassPathResource("GoodBye.drl",
                 DroolsTestPage.class), ResourceType.DRL);
-        
+
+
 
         if (kbuilder.hasErrors()) {
             logger.error(kbuilder.getErrors().toString());
@@ -65,7 +84,7 @@ public class DroolsTestPage extends PocMainPage {
         ksession.addEventListener(new DebugAgendaEventListener());
         ksession.addEventListener(new DebugWorkingMemoryEventListener());
 
-        
+
 
         add(new Label("message", "Drools Test Page"));
 
